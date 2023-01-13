37b629824ad30a3fcc437864d57f73ba70cc5f20
Timothy Miller <46549361+timothymiller@users.noreply.github.com>
2023-01-13 11:08:12 -0500

    Moved next folder

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,21 +1,24 @@
-import { StackContext, Api, NextjsSite } from 'sst/constructs'
+import { StackContext, NextjsSite } from 'sst/constructs'
 
 export function API({ stack, app }: StackContext) {
-  const api = new Api(stack, 'api', {
-    routes: {
-      'GET /': 'packages/functions/src/lambda.handler',
-    },
-  })
-  stack.addOutputs({
-    ApiEndpoint: api.url,
-  })
+  // const api = new Api(stack, 'api', {
+  //   routes: {
+  //     'GET /': 'packages/functions/src/lambda.handler',
+  //   },
+  // })
+  
 
   const site = new NextjsSite(stack, 'Site', {
-    path: 'frontend',
+    path: 'packages/frontend',
     environment: {
       // Pass the table details to our app
       REGION: app.region,
       // TABLE_NAME: table.tableName,
     },
+  })
+
+  stack.addOutputs({
+    // ApiEndpoint: api.url,
+    NextjsSite: site.url,
   })
 }
