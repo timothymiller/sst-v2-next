cc52948030ee580186fdcc7b247aaf0c79732944
Timothy Miller <46549361+timothymiller@users.noreply.github.com>
2023-01-13 10:58:47 -0500

    Added nextjs app

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,12 +1,21 @@
-import { StackContext, Api } from "sst/constructs";
+import { StackContext, Api, NextjsSite } from 'sst/constructs'
 
-export function API({ stack }: StackContext) {
-  const api = new Api(stack, "api", {
+export function API({ stack, app }: StackContext) {
+  const api = new Api(stack, 'api', {
     routes: {
-      "GET /": "packages/functions/src/lambda.handler",
+      'GET /': 'packages/functions/src/lambda.handler',
     },
-  });
+  })
   stack.addOutputs({
     ApiEndpoint: api.url,
-  });
+  })
+
+  const site = new NextjsSite(stack, 'Site', {
+    path: 'frontend',
+    environment: {
+      // Pass the table details to our app
+      REGION: app.region,
+      // TABLE_NAME: table.tableName,
+    },
+  })
 }
